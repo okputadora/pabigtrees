5298a901d8fcaf3ce3e29704cca066dde37aa395
okputadora <okputadora@gmail.com>
2020-05-24 11:12:34 -0400

    save colors as vars

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 import './banner.scss'
 
-const Header = () => (<div className="banner-container"><div className="banner">Champion Trees of Pennsylvania</div></div>)
+const Header = () => (
+  <div className="banner-container">
+    <div className="banner">
+      <div className="banner-title">Champion Trees of Pennsylvania</div>
+    </div>
+  </div>
+)
 
 export default Header
